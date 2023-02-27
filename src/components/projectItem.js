dda933b152e29b89884cd0d7071b30da0393be2a
dylancdavis <dylcdav@gmail.com>
2023-02-27 13:39:56 -0500

    Add delete button to project items

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useState } from "react"
 import ProjectBody from "./projectBody"
 import ProjectHeader from "./projectHeader"
+import discardIcon from '../assets/cross.png'
 
 const baseURL = 'http://localhost:3001'
 
@@ -15,16 +16,33 @@ const ProjectItem = ({id, title, initTasks}) => {
 		setTasks(newTaskList)
 	}
 
+	const onDiscard = () => {
+		console.log(`discard ${id}`)
+		
+	}
+
 	return (
 		<div className='project-item'>
 			<ProjectHeader>
 				<Title title={title}></Title>
 			</ProjectHeader>
 			<ProjectBody tasks={tasks} addTask={addTask} />
+			<DiscardButton onDiscard={onDiscard} />
 		</div>
 	)
 }
 
 const Title = ({title}) => <div className='project-title'>{title}</div>
+
+const DiscardButton = ({onDiscard}) => {
+	return (
+			<img 
+				className='discard' 
+				src={discardIcon}  
+				alt='discard icon'
+				onClick={onDiscard}>
+			</img>
+	)
+}
 
 export default ProjectItem
