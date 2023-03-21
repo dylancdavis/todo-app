82e77ab31ba37b4e396e8f231126112efd88c08d
dylancdavis <dylcdav@gmail.com>
2023-03-21 10:41:29 -0400

    Remove old color palettes

--- src/logic/colorHandler.js
+++ src/logic/colorHandler.js
@@ -2,8 +2,6 @@ import { parse } from "coolors-io";
 import chroma from "chroma-js";
 
 const palette = "https://coolors.co/f9fafb-060a0f-6593c2-6eb4a8-f0974e-e8765a";
-// const palette = 'https://coolors.co/ebf2fa-060a0e-064789-427aa1-679436-a5be00'
-// const palette = 'https://coolors.co/0f0806-f6f5f3-4281a4-48a9a6-d4b483-c1666b'
 
 const setColors = () => {
   let colors = parse(palette);
